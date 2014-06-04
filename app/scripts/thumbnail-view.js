ea11a38392e41a7196a67dc0ddcb7501e3ece023
Andy Georgia <andygeorgia@hotmail.com>
2014-06-04 14:12:36 -0400

    main layout

---
+++ app/scripts/thumbnail-view.js
@@ -0,0 +1,26 @@
+"use strict";
+
+var ThumbnailView = Backbone.View.extend({
+
+  className: 'thumbnail',
+
+  template: _.template($('.thumbnail-template').text()),
+
+  events: {
+    'click': 'showDetailView'
+  },
+
+  initialize: function(){
+    $('.container').append(this.el);
+    this.render()
+  },
+
+  render: function(){
+    var renderedTemplate = this.template(this.model.attributes)
+    this.$el.html(renderedTemplate)
+  },
+
+  showDetailView: function(){
+    new DetailView({model: this.model})
+  }
+})
